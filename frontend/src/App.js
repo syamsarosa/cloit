752425187b9fc35b809280c83876feda07c7ee43
ssarosa <syamsarosa@gmail.com>
2024-10-23 07:56:36 +0700

    Setup react-router-dom and layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/layout";
+import Menu from "./pages/menu";
+import ApiList from "./pages/api-list";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Menu />} />
+          {/* <Route index element={<Home />} /> */}
+          <Route path="menu" element={<Menu />} />
+          <Route path="api-list" element={<ApiList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
