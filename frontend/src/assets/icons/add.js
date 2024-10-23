1b33c4b0f25ea594c21ffe045d48b9b82ad7aafe
ssarosa <syamsarosa@gmail.com>
2024-10-23 07:58:16 +0700

    Add chevron-right, edit, add icon

---
+++ frontend/src/assets/icons/add.js
@@ -0,0 +1,17 @@
+import { cn } from "../../lib/utils";
+
+function Add({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      className={cn("fill-gray-500", className)}
+    >
+      <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+    </svg>
+  );
+}
+
+export default Add;
---
+++ frontend/src/assets/icons/chevron-right.js
@@ -0,0 +1,17 @@
+import { cn } from "../../lib/utils";
+
+function ChevronRight({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      className={cn("fill-gray-500", className)}
+    >
+      <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+    </svg>
+  );
+}
+
+export default ChevronRight;
